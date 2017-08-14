b2cb5b3770a6f8607781e98c3e3e29c96e5c2d26
kuroneko1996 <kuroneko1996@users.noreply.github.com>
2017-08-14 20:56:10 +0400

    update player animation make it use different directions

--- sprites.py
+++ sprites.py
@@ -45,27 +45,63 @@ class Player(pg.sprite.Sprite):
         self.y = y * TILE_SIZE
         self.vx, self.vy = 0, 0
         self.spd = 200
+        self.last_movex = 0.0
+        self.last_movey = 0.0
+        self.moving = False
 
         self.animation_timer = 0.0
-        self.idling_animation = Animation(0.50, PlayMode.LOOP,
-                                        game.spritesheet.get_image_alpha(0, 32, 32, 32),
-                                        game.spritesheet.get_image_alpha(32, 32, 32, 32))
-        self.walking_animation = Animation(0.10, PlayMode.LOOP,
-                                        game.spritesheet.get_image_alpha(64, 32, 32, 32),
-                                        game.spritesheet.get_image_alpha(96, 32, 32, 32),
-                                        game.spritesheet.get_image_alpha(128, 32, 32, 32))
+        self.idling_animation = Animation(
+            0.50, PlayMode.LOOP,
+            game.spritesheet.get_image_alpha(0, 32, 32, 32),
+            game.spritesheet.get_image_alpha(32, 32, 32, 32)
+        )
+        self.walking_animation_up = Animation(
+            0.10, PlayMode.LOOP,
+            game.spritesheet.get_image_alpha(64, 32, 32, 32),
+            game.spritesheet.get_image_alpha(96, 32, 32, 32),
+            game.spritesheet.get_image_alpha(128, 32, 32, 32)
+        )
+
+        self.walking_animation_down = Animation(
+            0.10, PlayMode.LOOP,
+            game.spritesheet.get_image_alpha(160, 32, 32, 32),
+            game.spritesheet.get_image_alpha(192, 32, 32, 32),
+            game.spritesheet.get_image_alpha(224, 32, 32, 32)
+        )
+
+        self.walking_animation_left = Animation(
+            0.10, PlayMode.LOOP,
+            game.spritesheet.get_image_alpha(64, 64, 32, 32),
+            game.spritesheet.get_image_alpha(96, 64, 32, 32),
+            game.spritesheet.get_image_alpha(128, 64, 32, 32)
+        )
+
+        self.walking_animation_right = Animation(
+            0.10, PlayMode.LOOP,
+            game.spritesheet.get_image_alpha(160, 64, 32, 32),
+            game.spritesheet.get_image_alpha(192, 64, 32, 32),
+            game.spritesheet.get_image_alpha(224, 64, 32, 32)
+        )
 
     def input(self):
         self.vx, self.vy = 0, 0
         keys = pg.key.get_pressed()
         if keys[pg.K_LEFT] or keys[pg.K_a]:
             self.vx = -self.spd
+            self.last_movex = -1
+            self.last_movey = 0
         if keys[pg.K_RIGHT] or keys[pg.K_d]:
             self.vx = self.spd
+            self.last_movex = 1
+            self.last_movey = 0
         if keys[pg.K_UP] or keys[pg.K_w]:
             self.vy = -self.spd
+            self.last_movey = -1
+            self.last_movex = 0
         if keys[pg.K_DOWN] or keys[pg.K_s]:
             self.vy = self.spd
+            self.last_movey = 1
+            self.last_movex = 0
 
         # diagonals
         if self.vx != 0 and self.vy != 0:
@@ -86,12 +122,26 @@ class Player(pg.sprite.Sprite):
         # update image rect
         self.rect.center = self.hit_rect.center
 
+        if self.vx != 0 or self.vy != 0:
+            self.moving = True
+        else:
+            self.moving = False
+
         self.update_animation(dt)
 
     def update_animation(self, dt):
         self.image = self.idling_animation.get_key_frame(self.animation_timer)
-        if self.vx != 0 or self.vy != 0:
-            self.image = self.walking_animation.get_key_frame(self.animation_timer)
+
+        if self.moving and self.last_movey != 0:
+            if self.last_movey > 0:
+                self.image = self.walking_animation_up.get_key_frame(self.animation_timer)
+            else:
+                self.image = self.walking_animation_down.get_key_frame(self.animation_timer)
+        elif self.moving and self.last_movex != 0:
+            if self.last_movex > 0:
+                self.image = self.walking_animation_right.get_key_frame(self.animation_timer)
+            else:
+                self.image = self.walking_animation_left.get_key_frame(self.animation_timer)
 
         self.animation_timer += dt
         if self.animation_timer >= 60:
