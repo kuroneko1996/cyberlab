29e1b76b82615d6f353d8f06cf9a4a74fd884023
Danil P <slemonide@gmail.com>
2017-08-15 14:30:10 -0700

    Added map source files that are editable in Tiled.

---
+++ assets/maps/sprites.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="spritesheet" tilewidth="32" tileheight="32" tilecount="48" columns="8">
+ <image source="../spritesheet.png" width="256" height="192"/>
+</tileset>
